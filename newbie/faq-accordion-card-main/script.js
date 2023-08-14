2019981125a4691565713552f8731059c4f6d306
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-08-14 16:31:26 +0300

    Set titles font-weight and <p> collapsing script

--- newbie/faq-accordion-card-main/script.js
+++ newbie/faq-accordion-card-main/script.js
@@ -1,24 +1,53 @@
 let questionTitles = document.querySelectorAll('button'),
     paragraphsList = document.querySelectorAll('p'),
-    qAndAList = document.querySelectorAll('.question');
+    qAndAList = document.querySelectorAll('.question'),
+    accordion = document.querySelector('.faqacordion');
 
 
 
-questionTitles.forEach((question) => {
+questionTitles.forEach((question) => { 
+
     question.addEventListener('click', () => {
         question.classList.toggle('weighted');
     })
 
     question.addEventListener('blur', () => {
-        question.classList.remove('weighted');
+        question.classList.remove('weighted'); 
     })
 });
 
 
-qAndAList.forEach((qAndA) => {
-    qAndA.addEventListener('click', () => {
-        qAndA.querySelector('p').remove('collapse');
-    })
+//    //calling function and return number of clicked item
+
+
+
+qAndAList.forEach((question, index) => {
+    let button = question.querySelector('button'),
+        paragraph = question.querySelector('p'),
+        picture = question.querySelector('.pic');
+
+    
+
+    button.addEventListener('click', (num1) => {
+        paragraph.classList.toggle('collapse');
+        picture.classList.toggle('pic_transform');
+    
+    removeOpen(index);
+    
+    });
+
 });
+
+function removeOpen(index1) {
+    qAndAList.forEach((item, index2) => {
+        let paragraph = item.querySelector('p');
+
+        if (index1 != index2) {
+            paragraph.classList.add('collapse');
+        }
+    })
+};
+
+
 
 

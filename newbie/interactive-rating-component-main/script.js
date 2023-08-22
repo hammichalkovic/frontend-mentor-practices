593fe28d70761f39e220b8a0cdd779359d72f09c
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-08-22 19:10:33 +0300

    Set result display on .thanks section

--- newbie/interactive-rating-component-main/script.js
+++ newbie/interactive-rating-component-main/script.js
@@ -1,35 +1,65 @@
 let marks = document.querySelectorAll('.mark'),
     submit = document.querySelector('.submit'),
     main = document.querySelector('.main'),
-    thanks = document.querySelector('.thanks');
+    thanks = document.querySelector('.thanks'),
+    selection = document.querySelector('.selection'),
+    result = '';
 
 
 
 marks.forEach((mark, index) => {
     mark.addEventListener('mouseenter', (e) => {
         e.target.classList.add('onmouseover');
+        
     });
 
     mark.addEventListener('mouseout', (e) => {
         mark.classList.remove('onmouseover');
     });
 
-    mark.addEventListener('click', (e) => {
-        mark.classList.toggle('markclicked');
+    mark.addEventListener('click', () => {
+        document.querySelector('.markclicked')?.classList.remove('mark');
+        mark.classList.add('markclicked');
+        
+        result = e.target.textContent;
+
     });
 
-    
 });
 
-// function toUnmark(index1) {
-//     marks.forEach((mark, index2) => {
-//         if(index1 != index2) {
-//             mark.classList.remove('markclicked');
-//         };
-//     });
-// };
+// marks.forEach((mark) => {
+//     mark.addEventListener('clickout', () => {
+//         mark.classList.remove('markclicked');
+//     })
+// });
+
+
+// https://www.youtube.com/watch?v=RBB2N341tr0  ПАСПРАБАВАЦЬ ЗРАБІЦЬ ПРАЗ МАСІЎ ПА ГЭТАЙ СПАСЫЛЦЫ
+
 
 submit.addEventListener('click', (e) => {
     main.classList.add('nodisplay');
     thanks.classList.remove('nodisplay');  
-})
+    selection.textContent = `You selected ${result} out of 5`;
+    
+});
+
+// ІДЭАЛЬНЫ СКРЫПТ ДЛЯ МАБІЛЬНАЙ ВЕРСІІ
+
+// marks.forEach((mark, index) => {
+//     mark.addEventListener('mouseenter', (e) => {
+//         e.target.classList.add('onmouseover');
+//         e.target.classList.add('markclicked');
+//     });
+
+//     mark.addEventListener('mouseout', (e) => {
+//         mark.classList.remove('onmouseover');
+//         mark.classList.remove('markclicked');
+//     });
+
+//     mark.addEventListener('click', (e) => {
+//         result = e.target.textContent;
+//     });
+
+// });
+

6f1e97e201068c73268d6eb1fd411b197c9f1306
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-09-04 17:20:15 +0300

    Set .shareoption open/close script and animation

--- newbie/article-preview-component-master/script.js
+++ newbie/article-preview-component-master/script.js
@@ -1,12 +1,20 @@
 let button = document.querySelector('.share'),
     buttonicon = button.querySelector('img'),
-    shareoptions = document.querySelector('.shareoptions');
+    shareoptions = document.querySelector('.shareoptions'),
+    buttonOffMobile = document.querySelector('.share_mobile_active');
 
 button.addEventListener('click', (e) => {
     button.classList.toggle('buttonactive');
     toChangeArrowColor();
     shareoptions.classList.toggle('nodisplay');
 
+});
+
+buttonOffMobile.addEventListener('click', (e) => {
+    shareoptions.classList.toggle('nodisplay');
+    if(buttonOffMobile.classList.contains('nodisplay')) {
+        buttonicon.src = './images/icon-share.svg';
+    }
 });
 
 function toChangeArrowColor() {

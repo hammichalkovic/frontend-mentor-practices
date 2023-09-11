4a657b581f0eed8804275258c2686c550bd43004
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-09-11 18:43:00 +0300

    Set submit event condition for email validation/not matching animation

--- newbie/base-apparel-coming-soon-master/script.js
+++ newbie/base-apparel-coming-soon-master/script.js
@@ -1,18 +1,29 @@
 let button = document.querySelector('button'),
     input = document.querySelector('input'),
-    error = document.querySelector('.errornotification');
+    error = document.querySelector('.errornotification'),
+    validRegex = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$";
+
 
 // function toNotifyError(e) {
 //     input.style.borderColor = 'hsl(0, 93%, 68%)';
 // }
 
-button.addEventListener('click', () => {
-    input.classList.add('bounce');
-    error.classList.remove('nodisplay');
-    setTimeout(refresh, 1000);
+
+
+button.addEventListener('click', (event) => {
+        if (!input.value.match(validRegex)) {
+        event.preventDefault();
+        input.classList.add('bounce');
+        error.classList.remove('nodisplay');
+        setTimeout(refresh, 1000);
+        };
 });
 
 function refresh() {
     input.classList.remove('bounce');
     error.classList.add('nodisplay');
 };
+
+window.addEventListener('click', (e) => {
+    console.log(e.target.attributes);
+})

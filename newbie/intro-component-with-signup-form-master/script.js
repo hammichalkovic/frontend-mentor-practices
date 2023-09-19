d8f46aa7a6f4fff67b83c20fc2be110165bad459
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-09-19 17:31:22 +0300

    Start writing script for input form

--- newbie/intro-component-with-signup-form-master/script.js
+++ newbie/intro-component-with-signup-form-master/script.js
@@ -1,2 +1,36 @@
-let inputs = document.querySelectorAll('input');
+let inputs = document.querySelectorAll('input'),
+    firstName = document.getElementById('firstname'),
+    lastName = document.getElementById('lastname'),
+    email = document.getElementById('email'),
+    password = document.getElementById('password'),
+    submit = document.querySelector('.submit');
+
+
+window.addEventListener('click', e => {
+    console.log(e.target.attributes);
+})
+
+inputs.forEach(input => {
+    submit.addEventListener('click', () => {
+
+        if (input.value == '') {
+            input.style.cssText = 'background: no-repeat url(./images/icon-error.svg); background-position: 95% 50%;';
+            // input.setAttribute('placeholder', '');
+            input.classList.add('bounce');
+            input.classList.add('error');
+            input.removeAttribute('placeholder');
+            setTimeout(refresh, 1000);
+        };
+
+    });
+
+    function refresh() {
+        input.classList.remove('bounce');
+        input.style.cssText = 'background: none;';
+    };
+});
+
+
+
+
 

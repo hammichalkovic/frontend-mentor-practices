178796dfd8ee28ae026f640c86a8e30041fc86bc
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-09-25 17:37:16 +0300

    Almost finish writing input events scripts

--- newbie/intro-component-with-signup-form-master/script.js
+++ newbie/intro-component-with-signup-form-master/script.js
@@ -6,61 +6,101 @@ let inputs = document.querySelectorAll('input'),
     submit = document.querySelector('.submit'),
     form = document.querySelector('.form'),
     spans = document.querySelectorAll('span'),
-    fieldset = document.querySelector('fieldset');
+    fieldset = document.querySelector('fieldset'),
+    validRegexEmail = '[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$';
 
 
 window.addEventListener('click', e => {
     console.log(e.target.attributes);
 })
 
-submit.addEventListener('click', toValidate);
+submit.addEventListener('click', toValidate1);
+submit.addEventListener('click', toValidate2);
+submit.addEventListener('click', toValidate3);
+submit.addEventListener('click', toValidate4);
 
-function toValidate(e) {
+
+function toValidate1(e) {
     
-
     let valid = true;
 
     if (!firstName.value) {
         e.preventDefault();
         form.classList.add('grow-tall-form');
         fieldset.classList.add('grow-tall-fieldset');
+        fieldset.classList.add('fieldset-even');
         let errorNotification1 = document.getElementById('11');
-        errorNotification1.classList.remove('nodisplay');
-        errorNotification1.setAttribute('aria-hidden', false);
-        errorNotification1.setAttribute('aria-invalid', true);
-        setTimeout(refreshHeight, 2000);   
-    } else if (!lastName.value) {
+         errorNotification1.classList.remove('nodisplay');
+         errorNotification1.setAttribute('aria-hidden', false);
+         errorNotification1.setAttribute('aria-invalid', true);
+         setTimeout(refreshHeight, 2000);   
+    } 
+    
+    return valid;
+};    
+    
+function toValidate2(e) {
+
+    let valid = true;
+
+    if (!lastName.value) {
         e.preventDefault();
-        let errorNotification2 = document.getElementById('22');
+        form.classList.add('grow-tall-form');
+        fieldset.classList.add('grow-tall-fieldset');
+        fieldset.classList.add('fieldset-even');
+        let errorNotification2 = document.getElementById('12');
         errorNotification2.classList.remove('nodisplay');
         errorNotification2.setAttribute('aria-hidden', false);
         errorNotification2.setAttribute('aria-invalid', true);
         setTimeout(refreshHeight, 2000);  
-    } else if (!email.value) {
+    };
+};
+
+
+function toValidate3(e) {
+
+    let valid = true;
+
+    if (!email.value || !email.value.match(validRegexEmail)) {
         e.preventDefault();
-        let errorNotification3 = document.getElementById('33');
+        form.classList.add('grow-tall-form');
+        fieldset.classList.add('grow-tall-fieldset');
+        fieldset.classList.add('fieldset-even');
+        let errorNotification3 = document.getElementById('13');
         errorNotification3.classList.remove('nodisplay');
         errorNotification3.setAttribute('aria-hidden', false);
         errorNotification3.setAttribute('aria-invalid', true);
         setTimeout(refreshHeight, 2000);  
-    } else if (!password.value) {
+    };
+};
+
+function toValidate4(e) {
+
+    let valid = true;
+
+     if (!password.value) {
         e.preventDefault();
-        let errorNotification4 = document.getElementById('44');
+        form.classList.add('grow-tall-form');
+        fieldset.classList.add('grow-tall-fieldset');
+        fieldset.classList.add('fieldset-even');
+        let errorNotification4 = document.getElementById('14');
         errorNotification4.classList.remove('nodisplay');
         errorNotification4.setAttribute('aria-hidden', false);
         errorNotification4.setAttribute('aria-invalid', true);
-        setTimeout(refreshHeight, 2000);
-         
+        setTimeout(refreshHeight, 2000);    
     }
 
     return valid;
 };
 
+
+
 spans.forEach(span => {
     submit.addEventListener('click', () => {
-        if (span.getAttribute('aria-hidden') == 'true') {
-        span.classList.remove('nodisplay');
+        if (span.getAttribute('aria-hidden') == 'false') {
+        // span.classList.remove('nodisplay');
         setTimeout(refreshSpans, 2000); 
+        setTimeout(refreshHeight, 2000);
         } 
 
     });
@@ -68,14 +108,20 @@ spans.forEach(span => {
 
 inputs.forEach(input => {
 
+    function toSetPhFirstName() {
+        input.setAttribute('placeholder', 'First Name');
+    }
+
     submit.addEventListener('click', () => {
-        if (!input.value) {
+        if (!input.value || !email.value.match(validRegexEmail)) {
             input.style.cssText = 'background: no-repeat url(./images/icon-error.svg); background-position: 95% 50%;';
             // input.setAttribute('placeholder', '');
             input.classList.add('bounce');
             input.classList.add('error');
             input.removeAttribute('placeholder');
+            email.style.color = 'hsl(0, 100%, 74%)';
             setTimeout(refresh, 2000);    
+            setTimeout(toSetPhFirstName, 2000);
         };
     });
 
@@ -95,6 +141,7 @@ function refreshSpans() {
 function refreshHeight() {
     form.classList.remove('grow-tall-form');
     fieldset.classList.remove('grow-tall-fieldset');
+    fieldset.classList.remove('fieldset-even');
 };
 
 

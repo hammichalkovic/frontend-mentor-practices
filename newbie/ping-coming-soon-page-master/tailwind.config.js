5a20002d5bb7fdebede0a32f5c079f97a5e03b3f
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-05 16:24:32 +0300

    Add colors in tailwind.config.js. Keep styling MV

--- newbie/ping-coming-soon-page-master/tailwind.config.js
+++ newbie/ping-coming-soon-page-master/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     fontFamily: {
       'body': ['"Libre Franklin"', 'sans-serif']
     },
+    colors: {
+      'gray': 'hsl(0, 0%, 59%)',
+      'very-dark-blue': 'hsl(209, 33%, 12%)',
+      'light-red': 'hsl(354, 100%, 66%)',
+      'pale-blue': 'hsl(223, 100%, 88%)',
+      'blue': 'hsl(223, 87%, 63%)'
+    },
     extend: {},
   },
   plugins: [],

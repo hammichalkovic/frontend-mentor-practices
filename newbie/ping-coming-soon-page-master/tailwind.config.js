7f1aad30ca803ac4630dc3534c6e74710b3ec6f4
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-06 16:35:20 +0300

    Keep styling MV

--- newbie/ping-coming-soon-page-master/tailwind.config.js
+++ newbie/ping-coming-soon-page-master/tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
       'body': ['"Libre Franklin"', 'sans-serif']
     },
     colors: {
+      'gray-transparent': 'hsla(0, 0%, 59%, 80%)',
       'gray': 'hsl(0, 0%, 59%)',
       'very-dark-blue': 'hsl(209, 33%, 12%)',
       'light-red': 'hsl(354, 100%, 66%)',
       'pale-blue': 'hsl(223, 100%, 88%)',
-      'blue': 'hsl(223, 87%, 63%)'
+      'blue': 'hsl(223, 87%, 63%)',
+      'white': 'hsl(0, 0%, 100%)'
     },
     extend: {},
   },

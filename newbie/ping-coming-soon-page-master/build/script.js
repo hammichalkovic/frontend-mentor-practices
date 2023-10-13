ea6fef79897473286469061439ee59b3bb78a33d
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-13 16:46:04 +0300

    Keep styling and writing script for error events

--- newbie/ping-coming-soon-page-master/build/script.js
+++ newbie/ping-coming-soon-page-master/build/script.js
@@ -1,12 +1,19 @@
 let input = document.querySelector('input'),
     submit = document.querySelector('button'),
-    validRegex = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$";
+    validRegex = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$",
+    alert = document.getElementById('alert');
 
 submit.addEventListener('click', (e) => {
     if (!input.value.match(validRegex)) {
         e.preventDefault();
-        input.classList.add('border-[hsl(354, 100%, 66%)]');
-        input.classList.remove('border-[hsl(223,100%,88%)]');
+        input.classList.add('border-light-red');
+        input.classList.add('animate-bounce');
+        input.classList.add('bounce');
+        alert.classList.remove('hidden');
+        submit.classList.add
     }
 });
 
+// subscribe = mt=[10px]
+// form = mt-[30px]
+// button = mt[40px]

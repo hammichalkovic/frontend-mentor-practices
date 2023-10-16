e312a0ac744154897f13805d48abd626780b5952
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-16 18:02:19 +0300

    Keep writing script for form validation error

--- newbie/ping-coming-soon-page-master/build/script.js
+++ newbie/ping-coming-soon-page-master/build/script.js
@@ -1,7 +1,11 @@
 let input = document.querySelector('input'),
     submit = document.querySelector('button'),
     validRegex = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$",
-    alert = document.getElementById('alert');
+    alert = document.getElementById('alert'),
+    form = document.querySelector('form'),
+    subscribe = document.getElementById('subscribe'),
+    signBox = document.querySelector('.signbox'),
+    illustration = document.querySelector('.illustration');
 
 submit.addEventListener('click', (e) => {
     if (!input.value.match(validRegex)) {
@@ -10,9 +14,16 @@ submit.addEventListener('click', (e) => {
         input.classList.add('animate-bounce');
         input.classList.add('bounce');
         alert.classList.remove('hidden');
-        submit.classList.add
+        submit.classList.add('mt-[50px]');
+        submit.classList.add('mb-[10px]');
+        form.classList.add('mt-[20px]');
+        input.classList.add('mt-[10px]');
+        subscribe.classList.add('mt-[10px]');
+        subscribe.classList.add('mb-[30px]');
+        signBox.classList.add('mt-[40px]');
     }
 });
+
 
 // subscribe = mt=[10px]
 // form = mt-[30px]

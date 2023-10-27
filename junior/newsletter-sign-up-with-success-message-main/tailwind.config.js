1a441272632e4fdfd3f3746694a8c29498058afc
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-27 15:42:01 +0300

    Create script.js. Mark-up main html elements for DTV. Start styling MV

--- junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
+++ junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./build/*.html'],
   theme: {
+    fontFamily: {
+      'body': ['"Roboto"', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],

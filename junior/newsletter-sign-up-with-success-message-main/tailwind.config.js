175c322931bf78332a47a380d638b9c7c8e2c28e
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-31 12:43:13 +0300

    Set color class for colors to use in tailwind.config.js

--- junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
+++ junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     fontFamily: {
       'body': ['"Roboto"', ...defaultTheme.fontFamily.sans]
     },
+    colors: {
+      'tomato': 'hsl(4, 100%, 67%)',
+      'dark-slate-grey': 'hsl(234, 29%, 20%)',
+      'charcoal-grey': 'hsl(235, 18%, 26%)',
+      'grey': 'hsl(231, 7%, 60%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
     extend: {},
   },
   plugins: [],

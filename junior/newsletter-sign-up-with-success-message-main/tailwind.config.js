9998ac8c48386eedfdd737913010b8749d405dd8
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-31 15:38:44 +0300

    Keep styling MV

--- junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
+++ junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -14,7 +14,11 @@ module.exports = {
       'grey': 'hsl(231, 7%, 60%)',
       'white': 'hsl(0, 0%, 100%)'
     },
-    extend: {},
+    extend: {
+      content: {
+        'checkmark': 'url("/assets/images/icon-list.svg")',
+      },
+    },
   },
   plugins: [],
 }

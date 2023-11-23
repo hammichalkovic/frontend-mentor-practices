e19f7053500d77f3838ac62c6bf88eed66a6de2d
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-11-23 17:17:23 +0300

    Fix validation event bug. Start writing script for error notification

--- junior/newsletter-sign-up-with-success-message-main/build/js/main.js
+++ junior/newsletter-sign-up-with-success-message-main/build/js/main.js
@@ -5,30 +5,45 @@ let signUpSection = document.querySelector('.sign-up'),
     emailInput = document.getElementById('email'),
     form = document.getElementById('form'),
     formChildren = form.querySelectorAll('.form-child'),
-    emailRegex = '[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$'; 
+    emailRegex = '[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$',
+    errorNtf = document.getElementById('error');
 
 
-emailInput.addEventListener('focus', () => {
-    emailInput.classList.add('input-focused');
-});
 
-confirmBtn.addEventListener('click', () => {
 
-    let valid = true;
 
-    if (!emailInput.value || !emailInput.value.match(emailRegex)) {
-        e.preventDefault();
+confirmBtn.addEventListener('click', (e) => {
+// to edit error notification event style next time
+    e.preventDefault();
+    errorNtf.classList.remove('hidden');
+    emailInput.classList.add('border-tomato');
+    setTimeout(emailInput.classList.remove('border-tomato'), 2000);
+    setTimeout(errorNtf.classList.add('hidden'), 2000);
+
+
+    if (emailInput.value.match(emailRegex)) {
+         
+         signUpSection.classList.add('hidden');
+        successMessage.classList.add('flex');
+        successMessage.classList.remove('hidden');  
     } 
 
-    return valid;
-    successMessage.classList.remove('hidden');  
-    signUpSection.classList.add('hidden');
     
+        // return valid;   
+        
+        
+        
+        // document.reload();
+    
+
 });
+
+
 
 returnBtn.addEventListener('click', () => {
     signUpSection.classList.remove('hidden');
     successMessage.classList.add('hidden'); 
+    successMessage.classList.remove('flex');
     emailInput.value = ''; 
 });
 

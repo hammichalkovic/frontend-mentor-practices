c6d5f5715f3f93ef3afdde9f112ae806725148a1
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-12-08 19:45:13 +0300

    Update tailwind.config.js

--- junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
+++ junior/newsletter-sign-up-with-success-message-main/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     extend: {
       content: {
-        'checkmark': 'url("assets/images/icon-list.svg")',
+        'checkmark': 'url("./assets/images/icon-list.svg")',
       },
     },
     keyframes: {

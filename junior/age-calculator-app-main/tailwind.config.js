dd642001c7ef92fced5512cfdc0fd6a50c37aea4
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-12-15 18:00:25 +0300

    Style default mobile version

--- junior/age-calculator-app-main/tailwind.config.js
+++ junior/age-calculator-app-main/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./build/*.html', './build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'san-serif'],  
+      },
+      colors: {
+        'offwhite': 'hsl(0, 0%, 94%)',
+        'lightgrey': 'hsl(0, 0%, 86%)',
+        'smokeygrey': 'hsl(0, 1%, 44%)',
+        'offblack': 'hsl(0, 0%, 8%)',
+        'purple': 'hsl(259, 100%, 65%)',
+        'lightred': 'hsl(0, 100%, 67%)'
+      }
+    },
   },
   plugins: [],
 }

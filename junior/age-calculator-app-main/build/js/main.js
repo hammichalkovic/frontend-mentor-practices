cff2a639dcd8456938a1d3dc9c9cd8e16c1d2c08
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-12-22 19:50:57 +0300

    Write main variables and  script for setting the date of birth

--- junior/age-calculator-app-main/build/js/main.js
+++ junior/age-calculator-app-main/build/js/main.js
@@ -2,10 +2,32 @@
 // sm:-mr-6 - for digits in result field in default state
 
 let desktopArrow = document.getElementById('arrow1'),
-    mobileArrow = document.getElementById('arrow2');
+    mobileArrow = document.getElementById('arrow2'),
+    dayVar = document.getElementById('day'),
+    monthVar = document.getElementById('month'),
+    yearVar = document.getElementById('year'),
+    resultYears = document.getElementById('result-years'),
+    resultMonths = document.getElementById('result-months'),
+    resultDays = document.getElementById('result-days'),
+    fullCurrentDate = new Date(),
+    currentDate = fullCurrentDate.getDate(),
+    currentMonth = fullCurrentDate.getMonth() + 1,
+    currentYear = fullCurrentDate.getFullYear();
+    
 
-if(window.matchMedia('(max-width: 450px)').matches) {
+
+if (window.matchMedia('(max-width: 450px)').matches) {
     desktopArrow.classList.remove('hidden');
 } else {
     mobileArrow.classList.remove('hidden');
+}
+
+function toGetBirthFullDate() {
+    let localFullCurrentDate = new Date();
+    localFullCurrentDate.setDate(dayVar.value * 1);
+    localFullCurrentDate.setMonth(monthVar.value * 1 - 1);
+    localFullCurrentDate.setFullYear(yearVar.value * 1);
+    localFullCurrentDate = localFullCurrentDate.toDateString();
+    console.log(localFullCurrentDate);
+    //to rewrite the function in promise-style
 }

0ed0fc06453ecc12e0ccd6e191874d3b2d6b0a03
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-02-19 18:37:44 +0300

    Set colors in tailwind.config.js

--- junior/news-homepage-main/tailwind.config.js
+++ junior/news-homepage-main/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./build/*.html', './build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'soft-orange': 'hsl(35, 77%, 62%)',
+        'soft-red': 'hsl(5, 85%, 63%)',
+        'offwhite': 'hsl(36, 100%, 99%)',
+        'grayish-blue': 'hsl(233, 8%, 79%)',
+        'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+        'very-dark-blue': 'hsl(240, 100%, 5%)'
+      }
+    },
   },
   plugins: [],
 }

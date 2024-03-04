f5c4ccce8ea20ed69e1907a4dbbf4d797979fd15
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-03-04 22:13:16 +0300

    Start styling header and main elements for desktop ver

--- junior/news-homepage-main/build/js/main.js
+++ junior/news-homepage-main/build/js/main.js
@@ -1,8 +1,13 @@
 let headerLinks = document.getElementById('header_link_list'),
-    interactionButton = document.getElementById('sidebar_interaction');
+    interactionButton = document.getElementById('sidebar_interaction'),
+    brightFutureImgDesktop = document.getElementById('bf_img_desktop'),
+    brightFutureImgMobile = document.getElementById('bf_img_mobile');
 
 
 if (!window.matchMedia('(max-width: 450px)').matches) {
     interactionButton.classList.add('hidden');
     headerLinks.classList.remove('hidden');
+    brightFutureImgMobile.classList.add('hidden');
+    brightFutureImgDesktop.classList.remove('hidden');
+
 } 

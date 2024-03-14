b9394f5f7a798c9d0359ab6f44dfd6b71a2d8af6
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-03-14 17:41:52 +0300

    Set click eventListener for linkButton

--- junior/news-homepage-main/build/js/main.js
+++ junior/news-homepage-main/build/js/main.js
@@ -1,4 +1,5 @@
 let headerLinks = document.getElementById('header_link_list'),
+    linkButton = document.getElementById('bright_future_button'),
     interactionButton = document.getElementById('sidebar_interaction'),
     brightFutureImgDesktop = document.getElementById('bf_img_desktop'),
     brightFutureImgMobile = document.getElementById('bf_img_mobile'),
@@ -13,6 +14,16 @@ if (!window.matchMedia('(max-width: 450px)').matches) {
     ;
 } 
 
-if(!window.matchMedia('(max-height: 1000px)').matches) {
+if(!window.matchMedia('(max-height: 1000px)').matches && !window.matchMedia('(max-width: 450px)').matches) {
     footer.classList.remove('hidden')
 }
+
+
+linkButton.addEventListener('click', () => {
+    linkButton.classList.add('bg-very-dark-blue');
+    linkButton.classList.remove('bg-soft-red');
+    setTimeout(() => {
+        linkButton.classList.add('bg-soft-red');
+        linkButton.classList.remove('bg-very-dark-blue');
+    }, 300)
+}) 

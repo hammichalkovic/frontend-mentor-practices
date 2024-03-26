2e5c1d9a891fe43902a53821f3beab191d99c8fa
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-03-26 20:06:23 +0300

    Set custom colors for project in tailwind.config.js

--- junior/notifications-page-main/tailwind.config.js
+++ junior/notifications-page-main/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./build/*.html', './build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-red': 'hsl(1, 90%, 64%)',
+        'custom-blue': 'hsl(219, 85%, 26%)',
+        'custom-white': 'hsl(0, 0%, 100%)',
+        'vlg-blue': 'hsl(210, 60%, 98%)',
+        'lg-blue-1': 'hsl(211, 68%, 94%)',
+        'lg-blue-2': 'hsl(205, 33%, 90%)',
+        'grayish-blue': 'hsl(219, 14%, 63%)',
+        'dark-grayish-blue': 'hsl(219, 12%, 42%)',
+        'very-dark-blue': 'hsl(224, 21%, 14%)'
+      }
+    },
   },
   plugins: [],
 }

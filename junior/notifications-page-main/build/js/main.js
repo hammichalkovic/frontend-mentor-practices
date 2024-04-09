e0989e188b1a62b3debc90cbba373c5a62c81f47
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-04-09 19:13:38 +0300

    Add eventListeners for groups and posts active states

--- junior/notifications-page-main/build/js/main.js
+++ junior/notifications-page-main/build/js/main.js
@@ -2,7 +2,7 @@ let footer = document.getElementById('footer'),
     main = document.getElementById('mainmain'),
     usernames = document.querySelectorAll('.username'),
     groups = document.querySelectorAll('.group'),
-    posts = document.querySelectorAll('.posts'),
+    posts = document.querySelectorAll('.post'),
     readAll = document.getElementById('read-all');
 
 
@@ -16,27 +16,39 @@ if (!window.matchMedia('(max-height: 950px)').matches && !window.matchMedia('(ma
 
 if (!window.matchMedia('(min-width: 640px)').matches) {
 
-    function toChangeColor(item, textDefault, textNew) {
-        item.classList.remove(textDefault);
-        item.classList.add(textNew);
-
-        
-           
-    }
-
-    function toReturnColor(item, textDefault, colorNew) {
-        item.classList.add(colorDefault);
-        item.classList.remove(colorNew);
+    function toChangeColor(item, textColorDefault, textColorNew) {
+        item.classList.remove(textColorDefault);
+        item.classList.add(textColorNew);      
     }
 
     usernames.forEach(username => {
         username.addEventListener('click', () => {
             toChangeColor(username, 'text-very-dark-blue', 'text-custom-blue');
-
             setTimeout(() => {
                 toChangeColor(username, 'text-custom-blue',  'text-very-dark-blue');
             }, 600)
 
         })
-    })
+    });
+
+    groups.forEach(group => {
+        group.addEventListener('click', () => {
+            toChangeColor(group, 'text-dark-grayish-blue', 'text-custom-blue');
+            setTimeout(() => {
+                toChangeColor(group, 'text-custom-blue',  'text-dark-grayish-blue');
+            }, 600)
+
+        })
+    });
+
+    posts.forEach(post => {
+        post.addEventListener('click', () => {
+            toChangeColor(post, 'text-dark-grayish-blue', 'text-custom-blue');
+            setTimeout(() => {
+                toChangeColor(post, 'text-custom-blue',  'text-dark-grayish-blue');
+            }, 600)
+
+        })
+    });
 }
+

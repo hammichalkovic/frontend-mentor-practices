b7202b27018e1b03c50be381868f9b74269dfc53
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-04-09 18:58:14 +0300

    Start writing script for mmq eventListeners

--- junior/notifications-page-main/build/js/main.js
+++ junior/notifications-page-main/build/js/main.js
@@ -1,5 +1,10 @@
 let footer = document.getElementById('footer'),
-    main = document.getElementById('mainmain');
+    main = document.getElementById('mainmain'),
+    usernames = document.querySelectorAll('.username'),
+    groups = document.querySelectorAll('.group'),
+    posts = document.querySelectorAll('.posts'),
+    readAll = document.getElementById('read-all');
+
 
 
 if (!window.matchMedia('(max-height: 950px)').matches && !window.matchMedia('(max-width: 450px)').matches) {
@@ -9,3 +14,29 @@ if (!window.matchMedia('(max-height: 950px)').matches && !window.matchMedia('(ma
     main.classList.remove('sm:mt-auto');
 }
 
+if (!window.matchMedia('(min-width: 640px)').matches) {
+
+    function toChangeColor(item, textDefault, textNew) {
+        item.classList.remove(textDefault);
+        item.classList.add(textNew);
+
+        
+           
+    }
+
+    function toReturnColor(item, textDefault, colorNew) {
+        item.classList.add(colorDefault);
+        item.classList.remove(colorNew);
+    }
+
+    usernames.forEach(username => {
+        username.addEventListener('click', () => {
+            toChangeColor(username, 'text-very-dark-blue', 'text-custom-blue');
+
+            setTimeout(() => {
+                toChangeColor(username, 'text-custom-blue',  'text-very-dark-blue');
+            }, 600)
+
+        })
+    })
+}

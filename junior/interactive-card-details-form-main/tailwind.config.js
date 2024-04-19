366df05d53982e3a95107aa2055915835ce34303
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-04-19 22:11:29 +0300

    Start styling mmq and set fontFamily

--- junior/interactive-card-details-form-main/tailwind.config.js
+++ junior/interactive-card-details-form-main/tailwind.config.js
@@ -1,8 +1,15 @@
+
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./build/*.html', './bulid/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'grotesk': ['"Space Grotesk"', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }

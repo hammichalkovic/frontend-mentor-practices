d418725a114f86a1ee3bd64fbc66c8705c85510f
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-04-24 20:08:55 +0300

    Set custom colors in tailwind.config.js

--- junior/interactive-card-details-form-main/tailwind.config.js
+++ junior/interactive-card-details-form-main/tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
         'grotesk': ['"Space Grotesk"', 'sans-serif']
       },
       colors: {
-        'gray-for-card': 'hsl(215, 11%, 75%)'
+        'gray-for-card': 'hsl(215, 11%, 75%)',
+        'err-red': 'hsl(0, 100%, 66%)',
+        'custom-white': 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 3%, 87%)',
+        'dark-grayish-violet': 'hsl(278, 68%, 11%)',
+        'very-dark-violet': 'hsl(278, 68%, 11%)'
       }
     },
   },

b5dbc088b02cd0151f32746b9d0ce3f5597d2462
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-04-29 21:23:57 +0300

    Keep styling <form>

--- junior/interactive-card-details-form-main/tailwind.config.js
+++ junior/interactive-card-details-form-main/tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
         'gray-for-card': 'hsl(215, 11%, 75%)',
         'err-red': 'hsl(0, 100%, 66%)',
         'custom-white': 'hsl(0, 0%, 100%)',
-        'light-grayish-violet': 'hsl(270, 10%, 50%)',
-        'lignt-grayish-violet-brd': 'hsl(270, 3%, 87%)',
+        'light-grayish-violet': 'hsl(270, 3%, 80%)',
+        'light-grayish-violet-brd': 'hsl(270, 3%, 87%)',
         'dark-grayish-violet': 'hsl(278, 68%, 11%)',
         'very-dark-violet': 'hsl(278, 68%, 11%)'
       }

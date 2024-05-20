c5d27fbe7231e6ef35c86577a3a5fa706805a220
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-05-20 14:59:04 +0300

    Wring main variables for elements in main.js

---
+++ junior/interactive-card-details-form-main/build/js/main.js
@@ -0,0 +1,12 @@
+let cardholderNameInput = document.getElementById('cardholder'),
+    cardNumberInput = document.getElementById('number'),
+    expMonthInput = document.getElementById('month'),
+    expYearInput = document.getElementById('year'),
+    cvcInput = document.getElementById('cvc'),
+    confirmBtn = document.getElementById('submit'),
+    cardholderNameDisp = document.getElementsByClassName('cardholder_name_place'),
+    cardNumberDisp = document.getElementsByClassName('number_place'),
+    expMonthDisp = document.getElementById('exp_month_place'),
+    expYearDisp = document.getElementById('exp_year_place'),
+    cvcDisp = document.getElementById('cvc_place');
+

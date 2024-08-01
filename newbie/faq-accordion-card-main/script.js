0e234626b1f2395a226afde26f49b3b10927c4fd
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-08-01 16:50:00 +0300

    Finish realigning desktop version. Keep writing script for radios

--- newbie/faq-accordion-card-main/script.js
+++ newbie/faq-accordion-card-main/script.js
@@ -2,47 +2,73 @@ let questionTitles = document.querySelectorAll('button'),
     paragraphsList = document.querySelectorAll('p'),
     qAndAList = document.querySelectorAll('.question'),
     accordion = document.querySelector('.faqacordion'),
-    arrows = document.querySelectorAll('.pic');
+    arrows = document.querySelectorAll('.pic'),
+    button1 = document.getElementById('button1'),
+    button2 = document.getElementById('button2'),
+    button3 = document.getElementById('button3'),
+    button4 = document.getElementById('button4'),
+    button5 = document.getElementById('button5'),
+    button6 = document.getElementById('button6'),
+    checksArr = [button1, button2, button3, button4, button5, button6];
 
 
-questionTitles.forEach((question) => { 
 
-    question.addEventListener('click', () => {
-        question.classList.toggle('weighted');
-    })
+    qAndAList.forEach(question => {
 
-    question.addEventListener('blur', () => {
-        question.classList.remove('weighted'); 
-    })
-});
+        let radio = question.firstElementChild.firstElementChild.firstElementChild.lastChild;    
 
+        
 
-qAndAList.forEach((question, index) => {
-    let button = question.querySelector('button'),
-        paragraph = question.querySelector('p'),
-        picture = question.querySelector('.pic');
+        question.addEventListener('click', (e) => {
+            e.stopPropagation();
+            console.log(radio);
+            radio.click();
 
-    
-
-    button.addEventListener('click', (num1) => {
-        paragraph.classList.toggle('collapse');
-        picture.classList.toggle('pic_transform');
-    
-    removeOpen(index);  //calling function and return number of clicked item
-    
+            if (radio.checked == 1) {
+                question.classList.toggle('weighted');
+                
+                // if (question.)
+            } });
     });
 
-});
+// questionTitles.forEach((question) => { 
 
-function removeOpen(index1) {
-    qAndAList.forEach((item, index2) => {
-        let paragraph = item.querySelector('p');
+//     question.addEventListener('click', () => {
+//         question.classList.add('weighted');
+//     })
 
-        if (index1 != index2) {
-            paragraph.classList.add('collapse');
-        }
-    })
-};
+//     question.addEventListener('blur', () => {
+//         question.classList.remove('weighted'); 
+//     })
+// });
+
+
+// qAndAList.forEach((question, index) => {
+//     let button = question.querySelector('button'),
+//         paragraph = question.querySelector('p'),
+//         picture = question.querySelector('.pic');
+
+    
+
+//     button.addEventListener('click', (num1) => {
+//         paragraph.classList.toggle('collapse');
+//         picture.classList.toggle('pic_transform');
+    
+//     removeOpen(index);  //calling function and return number of clicked item
+    
+//     });
+
+// });
+
+// function removeOpen(index1) {
+//     qAndAList.forEach((item, index2) => {
+//         let paragraph = item.querySelector('p');
+
+//         if (index1 != index2) {
+//             paragraph.classList.add('collapse');
+//         }
+//     })
+// };
 
 
 function toCloseAll() {
@@ -53,6 +79,8 @@ function toCloseAll() {
     arrows.forEach((arrow) => {
         arrow.classList.remove('pic_transform');
     });
+
+    button6.checked = 'true';
 };
 
 accordion.addEventListener('focusout', toCloseAll);

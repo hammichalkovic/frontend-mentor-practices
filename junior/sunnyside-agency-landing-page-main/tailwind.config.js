bb66c50b06d0a01555951040f1e30a23a53ab5f0
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-10-23 23:16:04 +0300

    Set fonts

--- junior/sunnyside-agency-landing-page-main/tailwind.config.js
+++ junior/sunnyside-agency-landing-page-main/tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./build/*.html', './build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fraunces: ['Fraunces', ...defaultTheme.fontFamily.sans],
+        barlow: ['Barlow', ...defaultTheme.fontFamily.sans] 
+      }
+    },
   },
   plugins: [],
 }

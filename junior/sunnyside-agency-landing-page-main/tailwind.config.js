b8719743a61b278433813a087d034749f363e699
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-11-04 22:15:00 +0300

    Fix some font styles and add custom colors

--- junior/sunnyside-agency-landing-page-main/tailwind.config.js
+++ junior/sunnyside-agency-landing-page-main/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       fontFamily: {
         fraunces: ['Fraunces', ...defaultTheme.fontFamily.sans],
         barlow: ['Barlow', ...defaultTheme.fontFamily.sans] 
+      }, 
+      color: {
+        'custom-soft-red': 'hsl(7, 99%, 70%)',
+        'custom-yellow': 'hsl(51, 100%, 49%)',
+        'custom-dark-cyan': 'hsl(167, 40%, 24%)',
+        'custom-dark-blue': 'hsl(198, 62%, 26%)',
+        'custom-moderate-cyan': 'hsl(168, 34%, 41%)',
+        'custom-very-dark-blue': 'hsl(212, 27%, 19%)',
+        'custom-very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+        'custom-dark-grayish-blue': 'hsl(232, 10%, 55%)',
+        'custom-grayish-blue': 'hsl(210, 4%, 67%)',
       }
     },
   },

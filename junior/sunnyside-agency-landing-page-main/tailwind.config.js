9031ba6cde6ace2168d706218054009ba485cc2f
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-11-04 23:00:23 +0300

    Fix extrend colors mistake and set colors for header. Add one more image

--- junior/sunnyside-agency-landing-page-main/tailwind.config.js
+++ junior/sunnyside-agency-landing-page-main/tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     extend: {
       fontFamily: {
         fraunces: ['Fraunces', ...defaultTheme.fontFamily.sans],
-        barlow: ['Barlow', ...defaultTheme.fontFamily.sans] 
+        barlow: ['Barlow', ...defaultTheme.fontFamily.sans],
       }, 
-      color: {
+      colors: {
         'custom-soft-red': 'hsl(7, 99%, 70%)',
         'custom-yellow': 'hsl(51, 100%, 49%)',
         'custom-dark-cyan': 'hsl(167, 40%, 24%)',
@@ -19,7 +19,7 @@ module.exports = {
         'custom-very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
         'custom-dark-grayish-blue': 'hsl(232, 10%, 55%)',
         'custom-grayish-blue': 'hsl(210, 4%, 67%)',
-      }
+      },
     },
   },
   plugins: [],
